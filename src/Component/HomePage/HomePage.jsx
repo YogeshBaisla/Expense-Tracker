db189f27277b532f52437887e2ac84d7bee2dec8
Yogesh Baisla <yogeshbaisla793@gmail.com>
2024-03-23 16:27:14 +0530

    Build Wallet compomnent ui

--- src/Component/HomePage/HomePage.jsx
+++ src/Component/HomePage/HomePage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import style from "./HomePage.module.css"
+import Wallet from "../Wallet/Wallet";
 
 export default function HomePage(){
     return(<div className={style.Container}>
         <h1>Expense Tracker</h1>
         <div>
             <div>div 1
-                <div>1.1</div>
+                <div><Wallet/></div>
                 <div>1.2</div>
                 <div>1.3</div>
             </div>
---
+++ src/Component/Wallet/Wallet.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./Wallet.module.css"
+export default function Wallet(){
+    return(<div className={style.Container}>
+        <div><h1>Wallet Balance : <span className={style.Amount}>₹{4500}</span></h1></div>
+        <button className={style.capsuleButton}>+Add Income</button>
+        </div>)
+}
